44997ba4f9a14de75f925b725f1d133bf582db39
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2021-07-20 14:58:18 -0300

    add new project package

---
+++ representation/src/main/java/com/project/representation/RepresentationApplication.java
@@ -0,0 +1,13 @@
+package com.project.representation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepresentationApplication.class, args);
+	}
+
+}
---
+++ representation/src/test/java/com/project/representation/RepresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.representation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
